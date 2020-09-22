269d9053eef2c85811837910171e0a263bb4e46d
ginpoko <ginpoko12161216@gmail.com>
2020-09-23 03:27:56 +0900

    home.htmlを追加

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/home/<string:user_name>/<int:age>')
+def home(user_name,age):
+    login_user = {
+        'name' : user_name,
+        'age' : age
+    }
+    return render_template('home.html', user_info=login_user)
 
 if __name__ == '__main__':
     app.run()
