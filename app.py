01eda35e7cfdb7e563119d532ff2aa6ac2767857
ginpoko <ginpoko12161216@gmail.com>
2020-09-23 05:37:22 +0900

    templates 3

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
-
+from datetime import datetime
 app = Flask(__name__)
 
+
+@app.template_filter('reverse_name')
+def reverse(name):
+    return name[-1::-1]
+
+@app.template_filter('born_year')
+def calucurate_born_year(age):
+    now_timestamp = datetime.now()
+    return str(now_timestamp.year - int(age) )+ '年'
 
 @app.route('/')
 def index():
@@ -15,12 +24,22 @@ def home(user_name,age):
     }
     return render_template('home.html', user_info=login_user)
 
+class UserInfo:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
 @app.route('/userlist')
 def user_list():
-    users = [
-        'Taro', 'Jiro', 'Saburo', 'Shiro','Hanako'
-    ]
+    # users = [
+    #     'Taro', 'Jiro', 'Saburo', 'Shiro','Hanako'
+    # ]
+    users =[
+        UserInfo('Taro',21), UserInfo('Jiro',32), UserInfo('Hanako',43)
+        ]
     is_login = True
     return render_template('userlist.html', users=users, is_login=is_login)
+
 if __name__ == '__main__':
     app.run(debug=True)
