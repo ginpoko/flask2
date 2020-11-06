8aca761856412ad347740e2ea27f1fb08d812334
{ginpoko} <{ginpoko12161216@gmail.com}>
2020-11-06 19:20:03 +0900

    templateフィルター2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, abort
 from datetime import datetime
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def reverse(name):
 @app.template_filter('born_year')
 def calucurate_born_year(age):
     now_timestamp = datetime.now()
-    return str(now_timestamp.year - int(age) )+ '年'
+    return str(now_timestamp.year - int(age) ) + '年'
 
 @app.route('/')
 def index():
@@ -40,6 +40,24 @@ def user_list():
         ]
     is_login = True
     return render_template('userlist.html', users=users, is_login=is_login)
+
+
+@app.route('/user/<string:user_name>/<int:age>')
+def user(user_name, age):
+    if user_name in ['Taro', 'Jiro', 'Saburo']:
+        return redirect(url_for('home',user_name=user_name, age=age))
+    else:
+        abort(500, 'そのユーザはリダイレクトできません')
+
+
+@app.errorhandler(500)
+def system_error(error):
+    error_description = error.description
+    return render_template('system_error.html',error_description=error_description), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('not_found.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
